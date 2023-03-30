9f4c2157082c67e54ee6e41950208c0b496df14c
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-31 00:12:13 +0200

    Updates

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from './Home'
-export { default as Page } from './Page'
-export { default as Projects } from './Projects'
-export { default as Education } from './Education'
-export { default as ErrorPage } from './ErrorPage'
-export { default as Contact } from './Contact'
