3d28dc37946bec5949cf2b41e143f6387b3cf208
JordiNodejs <jordi.nodejs@gmail.com>
2023-06-26 17:17:23 +0200

    illustration

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,12 +2,12 @@ function Home() {
   return (
     <>
       <section className="h-screen relative flex items-center max-w-[1000px] mx-auto">
-        <div className="p-0 h-full text-center max-w-full page_header max-[700px]:after:hidden">
-          <div className="flex items-center justify-center w-full border-2 max-sm:flex-col border-cyan-700">
-            <figure className="w-2/5 border-2 sm:order-2 lg:w-4/5 border-cyan-600">
+        <div className="relative flex h-full max-w-full p-0 py-16 text-center">
+          <div className="flex items-center justify-center w-full max-sm:flex-col">
+            <figure className="w-2/5 md:w-4/5">
               <picture>
                 <img
-                  src="assets/avatar-square-padding.png"
+                  src="assets/slider.svg"
                   alt="Avatar"
                 />
               </picture>
@@ -17,7 +17,7 @@ function Home() {
               React
             </figcaption>
             <div className="flex flex-col gap-3 max-[700px]:px-2">
-              <h1 className="font-extrabold relative text-7xl inline-block tracking-tighter text-transparent md:text-8xl bg-clip-text bg-gradient-to-b dark:from-[#fde089] dark:to-[#ec8b26] from-[#ffd24e] to-[#b96205]">
+              <h1 className="font-extrabold relative text-7xl  inline-block tracking-tighter text-transparent md:text-6xl bg-clip-text bg-gradient-to-b dark:from-[#fde089] dark:to-[#ec8b26] from-[#ffd24e] to-[#b96205]">
                 <span className="inline-flex shake-left-right">
                   <img
                     src="assets/wave.png"
