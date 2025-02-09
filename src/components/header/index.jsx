01d51265be4b5f56d809b0cde37a82d3cb96739a
= <=>
2025-02-09 17:18:24 +0100

    refactor Header component to utilize external JSON data for dynamic content

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import NavHeader from './nav'
 import FooterHeader from './footer'
+import presentationData from '../../db/presentation.json'
 
 export default function Header() {
   return (
@@ -7,15 +8,13 @@ export default function Header() {
       <div className="flex flex-col py-24 px-14 max-lg:px-2">
         <div className="flex flex-col gap-3 text-emerald-800/80 dark:text-emerald-400/60">
           <h1 className="font-bold relative items-start text-6xl inline-block tracking-wider">
-            J-.ORCAJO
+            {presentationData.name}
           </h1>
           <h2 className="items-end text-xl tracking-tighter">
-            Software Engineer
+            {presentationData.title}
           </h2>
           <p className="w-96 max-[700px]:w-full items-start leading-5">
-            +2 years of experience in the programming world, specializing in
-            React. Committed to developing accessible and high-performance user
-            experiences.
+            {presentationData.description}
           </p>
         </div>
         <NavHeader />
